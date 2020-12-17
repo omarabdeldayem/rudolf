16b3d4a825577d275287a333ce4bfdc7e598afa6
Omar Abdeldayem <omar.abdeldayem@outlook.com>
2020-12-17 07:35:38 -0600

    Baby's first ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use ndarray::prelude::*;
+
+fn update(x: ArrayView1<u32>, u: ArrayView1<u32>) -> std::io::Result<()> {
+    Ok(())
+}
+
+fn predict() -> std::io::Result<()> {
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = array![1, 2, 3];
+    let b = array![4, 5, 6];
+    let c = update(a.view(), b.view());
+
+    println!("{:?}", a);
+    println!("{:?}", b);
+    println!("{:?}", c);
 }
