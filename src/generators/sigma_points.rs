2ed3f2e2b21d103b469e9ea221c9a5591170b702
Omar Abdeldayem <omar.abdeldayem@outlook.com>
2021-01-03 07:04:50 -0600

    Generate sigma points for MerweGenerator

--- src/generators/sigma_points.rs
+++ src/generators/sigma_points.rs
@@ -1,16 +1,35 @@
 extern crate nalgebra as na;
 
-use na::linalg::Cholesky;
 use na::allocator::Allocator;
-use na::{DefaultAllocator, Dim, DimName, MatrixN, RealField, VectorN};
+use na::linalg::Cholesky;
+use na::{DefaultAllocator, Dim, DimName, MatrixMN, MatrixN, RealField, VectorN};
 
-pub trait SigmaPointGenerator<T, D>
+pub struct SigmaPoints<T, DState, DSigma>
 where
     T: RealField,
-    D: Dim + DimName,
-    DefaultAllocator: Allocator<T, D> + Allocator<T, D, D>,
+    DState: Dim + DimName,
+    DSigma: Dim + DimName,
+    DefaultAllocator: Allocator<T, DSigma> + Allocator<T, DSigma, DState>,
 {
-    fn generate_sigmas(&self, mean: &VectorN<T, D>, cov: &MatrixN<T, D>) -> VectorN<T, D>;
+    pub weights: VectorN<T, DSigma>,
+    pub sigmas: MatrixMN<T, DSigma, DState>,
+}
+
+pub trait SigmaPointGenerator<T, DState, DSigma>
+where
+    T: RealField,
+    DState: Dim + DimName,
+    DSigma: Dim + DimName,
+    DefaultAllocator: Allocator<T, DState>
+        + Allocator<T, DState, DState>
+        + Allocator<T, DSigma, DState>
+        + Allocator<T, DSigma>,
+{
+    fn generate_sigmas(
+        &self,
+        mean: &VectorN<T, DState>,
+        cov: &MatrixN<T, DState>,
+    ) -> SigmaPoints<T, DState, DSigma>;
     fn generate_weights(&self);
 }
 
@@ -21,20 +40,18 @@ where
     pub kappa: T,
 }
 
-impl<T, D> SigmaPointGenerator<T, D> for SimplexGenerator<T>
-where
-    T: RealField,
-    D: Dim + DimName,
-    DefaultAllocator: Allocator<T, D> + Allocator<T, D, D>,
-{
-    fn generate_sigmas(&self, mean: &VectorN<T, D>, cov: &MatrixN<T, D>) -> VectorN<T, D> {
-        let sqrt_cov = Cholesky::<T, D>::new(*cov);
-    }
+// impl<T, D> SigmaPointGenerator<T, D> for SimplexGenerator<T>
+// where
+//     T: RealField,
+//     D: Dim + DimName,
+//     DefaultAllocator: Allocator<T, D> + Allocator<T, D, D>,
+// {
+//     fn generate_sigmas(&self, mean: &VectorN<T, D>, cov: &MatrixN<T, D>) -> VectorN<T, D> {
+//         let sqrt_cov = Cholesky::<T, D>::new(*cov);
+//     }
 
-    fn generate_weights(&self) {
-
-    }
-}
+//     fn generate_weights(&self) {}
+// }
 
 pub struct JulierGenerator<T>
 where
@@ -43,39 +60,64 @@ where
     pub scale_factor: T,
 }
 
-impl<T, D> SigmaPointGenerator<T, D> for JulierGenerator<T>
-where
-    T: RealField,
-    D: Dim + DimName,
-    DefaultAllocator: Allocator<T, D> + Allocator<T, D, D>,
-{
-    fn generate_sigmas(&self, mean: &VectorN<T, D>, cov: &MatrixN<T, D>) -> VectorN<T, D> {
-        let sqrt_cov = Cholesky::<T, D>::new(*cov);
-    }
+// impl<T, D> SigmaPointGenerator<T, D> for JulierGenerator<T>
+// where
+//     T: RealField,
+//     D: Dim + DimName,
+//     DefaultAllocator: Allocator<T, D> + Allocator<T, D, D>,
+// {
+//     fn generate_sigmas(&self, mean: &VectorN<T, D>, cov: &MatrixN<T, D>) -> VectorN<T, D> {
+//         let sqrt_cov = Cholesky::<T, D>::new(*cov);
+//     }
 
-    fn generate_weights(&self) {
-
-    }
-}
+//     fn generate_weights(&self) {}
+// }
 
 pub struct MerweGenerator<T>
 where
     T: RealField,
 {
     pub alpha: T,
+    pub beta: T,
+    pub kappa: T,
+    pub dim: T,
 }
 
-impl<T, D> SigmaPointGenerator<T, D> for MerweGenerator<T>
+impl<T, DState, DSigma> SigmaPointGenerator<T, DState, DSigma> for MerweGenerator<T>
 where
     T: RealField,
-    D: Dim + DimName,
-    DefaultAllocator: Allocator<T, D> + Allocator<T, D, D>,
+    DState: Dim + DimName,
+    DSigma: Dim + DimName,
+    DefaultAllocator: Allocator<T, DState>
+        + Allocator<T, DState, DState>
+        + Allocator<T, DSigma, DState>
+        + Allocator<T, DSigma>,
 {
-    fn generate_sigmas(&self, mean: &VectorN<T, D>, cov: &MatrixN<T, D>) -> VectorN<T, D> {
-        let sqrt_cov = Cholesky::<T, D>::new(*cov);
+    fn generate_sigmas(&self, mean: &VectorN<T, DState>, cov: &MatrixN<T, DState>) -> SigmaPoints<T, DState, DSigma> {
+        let lambda = (self.alpha * self.alpha) * (self.dim + self.kappa) - self.dim;
+        let scaled_cov = cov.scale(self.dim + lambda);
+        let sqrt_cov = Cholesky::<T, DState>::new(scaled_cov).unwrap().unpack();
+        let mut sigmas = MatrixMN::<T, DSigma, DState>::from_row_slice(mean.as_slice());
+
+        //
+        for i in 1..mean.len() {
+            let mut slice = sigmas.row_mut(i);
+            slice += sqrt_cov.row(i - 1);
+        }
+
+        //
+        for i in mean.len()..sigmas.nrows() {
+            let mut slice = sigmas.row_mut(i);
+            slice -= sqrt_cov.row(i - mean.len());
+        }
+
+        let weights = VectorN::<T, DSigma>::zeros();
+
+        SigmaPoints::<T, DState, DSigma> {
+            weights: weights,
+            sigmas: sigmas,
+        }
     }
 
-    fn generate_weights(&self) {
-
-    }
+    fn generate_weights(&self) {}
 }
